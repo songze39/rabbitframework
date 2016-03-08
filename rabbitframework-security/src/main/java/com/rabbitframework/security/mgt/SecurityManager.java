72f620ad9188e258112b5a95677dc7a8d9f2f412
liangjy <28554768@qq.com>
2016-03-08 21:28:53 +0800

    add

--- rabbitframework-security/src/main/java/com/rabbitframework/security/mgt/SecurityManager.java
+++ rabbitframework-security/src/main/java/com/rabbitframework/security/mgt/SecurityManager.java
@@ -1,11 +1,14 @@
 package com.rabbitframework.security.mgt;
 
+import com.rabbitframework.security.authc.Authenticator;
 import com.rabbitframework.security.authz.Authorizer;
 import com.rabbitframework.security.session.mgt.SessionManager;
 
 
 /**
+ * 安全管理接口
  * @author justin.liang
  */
-public interface SecurityManager extends Authorizer, SessionManager {
+public interface SecurityManager extends Authenticator, Authorizer, SessionManager {
+
 }
