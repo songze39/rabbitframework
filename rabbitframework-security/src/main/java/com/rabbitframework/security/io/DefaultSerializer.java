bb0abec53136cd84ab1d15c4ef870d341cb45e51
liangjy <28554768@qq.com>
2016-04-01 10:58:03 +0800

    update

--- rabbitframework-security/src/main/java/com/rabbitframework/security/io/DefaultSerializer.java
+++ rabbitframework-security/src/main/java/com/rabbitframework/security/io/DefaultSerializer.java
@@ -21,65 +21,84 @@ package com.rabbitframework.security.io;
 import java.io.*;
 
 /**
- * Serializer implementation that uses the default JVM serialization mechanism (Object Input/Output Streams).
+ * Serializer implementation that uses the default JVM serialization mechanism
+ * (Object Input/Output Streams).
  *
  * @since 0.9
  */
 public class DefaultSerializer<T> implements Serializer<T> {
 
-    /**
-     * This implementation serializes the Object by using an {@link ObjectOutputStream} backed by a
-     * {@link ByteArrayOutputStream}.  The {@code ByteArrayOutputStream}'s backing byte array is returned.
-     *
-     * @param o the Object to convert into a byte[] array.
-     * @return the bytes representing the serialized object using standard JVM serialization.
-     * @throws SerializationException wrapping a {@link IOException} if something goes wrong with the streams.
-     */
-    public byte[] serialize(T o) throws SerializationException {
-        if (o == null) {
-            String msg = "argument cannot be null.";
-            throw new IllegalArgumentException(msg);
-        }
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        BufferedOutputStream bos = new BufferedOutputStream(baos);
+	/**
+	 * This implementation serializes the Object by using an
+	 * {@link ObjectOutputStream} backed by a {@link ByteArrayOutputStream}. The
+	 * {@code ByteArrayOutputStream}'s backing byte array is returned.
+	 *
+	 * @param o
+	 *            the Object to convert into a byte[] array.
+	 * @return the bytes representing the serialized object using standard JVM
+	 *         serialization.
+	 * @throws SerializationException
+	 *             wrapping a {@link IOException} if something goes wrong with
+	 *             the streams.
+	 */
+	public byte[] serialize(T o) throws SerializationException {
+		if (o == null) {
+			String msg = "argument cannot be null.";
+			throw new IllegalArgumentException(msg);
+		}
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		BufferedOutputStream bos = new BufferedOutputStream(baos);
 
-        try {
-            ObjectOutputStream oos = new ObjectOutputStream(bos);
-            oos.writeObject(o);
-            oos.close();
-            return baos.toByteArray();
-        } catch (IOException e) {
-            String msg = "Unable to serialize object [" + o + "].  " +
-                    "In order for the DefaultSerializer to serialize this object, the [" + o.getClass().getName() + "] " +
-                    "class must implement java.io.Serializable.";
-            throw new SerializationException(msg, e);
-        }
-    }
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			oos.writeObject(o);
+			oos.close();
+			return baos.toByteArray();
+		} catch (IOException e) {
+			String msg = "Unable to serialize object [" + o + "].  "
+					+ "In order for the DefaultSerializer to serialize this object, the [" + o.getClass().getName()
+					+ "] " + "class must implement java.io.Serializable.";
+			throw new SerializationException(msg, e);
+		}
+	}
 
-    /**
-     * This implementation deserializes the byte array using a {@link ObjectInputStream} using a source
-     * {@link ByteArrayInputStream} constructed with the argument byte array.
-     *
-     * @param serialized the raw data resulting from a previous {@link #serialize(Object) serialize} call.
-     * @return the deserialized/reconstituted object based on the given byte array
-     * @throws SerializationException if anything goes wrong using the streams.
-     */
-    public T deserialize(byte[] serialized) throws SerializationException {
-        if (serialized == null) {
-            String msg = "argument cannot be null.";
-            throw new IllegalArgumentException(msg);
-        }
-        ByteArrayInputStream bais = new ByteArrayInputStream(serialized);
-        BufferedInputStream bis = new BufferedInputStream(bais);
-        try {
-            ObjectInputStream ois = new ClassResolvingObjectInputStream(bis);
-            @SuppressWarnings({"unchecked"})
-            T deserialized = (T) ois.readObject();
-            ois.close();
-            return deserialized;
-        } catch (Exception e) {
-            String msg = "Unable to deserialze argument byte array.";
-            throw new SerializationException(msg, e);
-        }
-    }
+	/**
+	 * This implementation deserializes the byte array using a
+	 * {@link ObjectInputStream} using a source {@link ByteArrayInputStream}
+	 * constructed with the argument byte array.
+	 *
+	 * @param serialized
+	 *            the raw data resulting from a previous
+	 *            {@link #serialize(Object) serialize} call.
+	 * @return the deserialized/reconstituted object based on the given byte
+	 *         array
+	 * @throws SerializationException
+	 *             if anything goes wrong using the streams.
+	 */
+	public T deserialize(byte[] serialized) throws SerializationException {
+		if (serialized == null) {
+			String msg = "argument cannot be null.";
+			throw new IllegalArgumentException(msg);
+		}
+		ByteArrayInputStream bais = new ByteArrayInputStream(serialized);
+		BufferedInputStream bis = new BufferedInputStream(bais);
+		ObjectInputStream ois = null;
+		try {
+			ois = new ClassResolvingObjectInputStream(bis);
+			@SuppressWarnings({ "unchecked" })
+			T deserialized = (T) ois.readObject();
+			return deserialized;
+		} catch (Exception e) {
+			String msg = "Unable to deserialze argument byte array.";
+			throw new SerializationException(msg, e);
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
 }
