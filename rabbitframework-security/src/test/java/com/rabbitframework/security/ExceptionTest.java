f25f5e428001ce3a100a18ebe6093078f4b795a0
liangjy <28554768@qq.com>
2016-04-03 21:56:58 +0800

    update

--- rabbitframework-security/src/test/java/com/rabbitframework/security/ExceptionTest.java
+++ rabbitframework-security/src/test/java/com/rabbitframework/security/ExceptionTest.java
@@ -24,31 +24,29 @@ import org.junit.Test;
 
 import com.rabbitframework.security.util.ClassUtils;
 
-
 /**
  */
-@SuppressWarnings({"ThrowableInstanceNeverThrown"})
 public abstract class ExceptionTest extends TestCase {
 
-    protected abstract Class getExceptionClass();
+	protected abstract Class<?> getExceptionClass();
 
-    @Test
-    public void testNoArgConstructor() {
-        ClassUtils.newInstance(getExceptionClass());
-    }
+	@Test
+	public void testNoArgConstructor() {
+		ClassUtils.newInstance(getExceptionClass());
+	}
 
-    @Test
-    public void testMsgConstructor() throws Exception {
-        ClassUtils.newInstance(getExceptionClass(), "Msg");
-    }
+	@Test
+	public void testMsgConstructor() throws Exception {
+		ClassUtils.newInstance(getExceptionClass(), "Msg");
+	}
 
-    @Test
-    public void testCauseConstructor() throws Exception {
-        ClassUtils.newInstance(getExceptionClass(), new Throwable());
-    }
+	@Test
+	public void testCauseConstructor() throws Exception {
+		ClassUtils.newInstance(getExceptionClass(), new Throwable());
+	}
 
-    @Test
-    public void testMsgCauseConstructor() {
-        ClassUtils.newInstance(getExceptionClass(), "Msg", new Throwable());
-    }
+	@Test
+	public void testMsgCauseConstructor() {
+		ClassUtils.newInstance(getExceptionClass(), "Msg", new Throwable());
+	}
 }
