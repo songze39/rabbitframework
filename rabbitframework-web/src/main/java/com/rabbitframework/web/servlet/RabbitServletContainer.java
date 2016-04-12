ea579a701e2f11f50312ebf581ef4557ecc83dda
liangjy <28554768@qq.com>
2016-04-12 22:25:29 +0800

    update

--- rabbitframework-web/src/main/java/com/rabbitframework/web/servlet/RabbitServletContainer.java
+++ rabbitframework-web/src/main/java/com/rabbitframework/web/servlet/RabbitServletContainer.java
@@ -1,7 +1,24 @@
 package com.rabbitframework.web.servlet;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class RabbitServletContainer extends ServletContainer {
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create Jersey Servlet container.
+	 */
+	public RabbitServletContainer() {
+		super();
+	}
+
+	/**
+	 * Create Jersey Servlet container.
+	 *
+	 * @param resourceConfig container configuration.
+	 */
+	public RabbitServletContainer(final ResourceConfig resourceConfig) {
+		super(resourceConfig);
+	}
 }
