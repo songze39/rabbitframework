a371935e45b999caadfce54fe35f5ba64bb1adda
liangjy <28554768@qq.com>
2016-04-13 22:54:25 +0800

    update

---
+++ rabbitframework-commons/src/main/java/com/rabbitframework/commons/utils/UuidGenerator.java
@@ -0,0 +1,58 @@
+package com.rabbitframework.commons.utils;
+import com.fasterxml.uuid.EthernetAddress;
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.NoArgGenerator;
+
+public class UUIDGenerator {
+    public static String DEFAULT_UUID36 = "00000000-0000-0000-0000-000000000000";
+    public static String DEFAULT_UUID32 = "00000000000000000000000000000000";
+    private static NoArgGenerator timeGenerator;
+    private static NoArgGenerator randomGenerator;
+
+    static {
+        ensureGeneratorInitialized();
+    }
+
+    private static void ensureGeneratorInitialized() {
+        if (timeGenerator == null) {
+            synchronized (UUIDGenerator.class) {
+                if (timeGenerator == null) {
+                    timeGenerator = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
+                }
+            }
+        }
+        if (randomGenerator == null) {
+            synchronized (com.fasterxml.uuid.UUIDGenerator.class) {
+                if (randomGenerator == null) {
+                    randomGenerator = Generators.randomBasedGenerator();
+                }
+            }
+        }
+    }
+
+    public static String getRandomUUID36() {
+        return randomGenerator.generate().toString();
+    }
+
+    public static String getRandomUUID32() {
+        return getUUID32(getRandomUUID36());
+    }
+
+
+    public static String getTimeUUID36() {
+        return timeGenerator.generate().toString();
+    }
+
+    public static String getTimeUUID32() {
+        return getUUID32(timeGenerator.generate().toString());
+    }
+
+
+    private static String getUUID32(String generator) {
+        return generator.replace("-", "");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getTimeUUID36());
+    }
+}
