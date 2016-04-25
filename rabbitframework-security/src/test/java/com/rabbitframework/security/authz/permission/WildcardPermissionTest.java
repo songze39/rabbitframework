2f82cb9fc9e18a809185f4a18e40236400a4a2a8
liangjy <28554768@qq.com>
2016-04-25 21:30:01 +0800

    del

--- rabbitframework-security/src/test/java/com/rabbitframework/security/authz/permission/WildcardPermissionTest.java
+++ rabbitframework-security/src/test/java/com/rabbitframework/security/authz/permission/WildcardPermissionTest.java
@@ -11,15 +11,16 @@ import com.rabbitframework.security.authz.Permission;
 import com.rabbitframework.security.authz.permission.WildcardPermission;
 
 public class WildcardPermissionTest {
-	private Logger logger = LoggerFactory.getLogger(WildcardPermissionTest.class);
+	private Logger logger = LoggerFactory
+			.getLogger(WildcardPermissionTest.class);
 
 	@Test
 	public void testParts() {
-		//结果需要精准匹配
-		WildcardPermission permission = new WildcardPermission("system:user:del");
+		// 结果需要精准匹配
+		WildcardPermission permission = new WildcardPermission("/index:/index/test");
 		List<Permission> perminfo = new ArrayList<>();
-		perminfo.add(new WildcardPermission("system:user:del"));
-		perminfo.add(new WildcardPermission("system:user:update"));
+		perminfo.add(new WildcardPermission("/index/test"));
+		perminfo.add(new WildcardPermission("/index"));
 		for (Permission permission2 : perminfo) {
 			logger.info("result:" + permission2.implies(permission));
 		}
