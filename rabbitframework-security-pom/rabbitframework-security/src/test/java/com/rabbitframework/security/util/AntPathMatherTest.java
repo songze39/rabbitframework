5998561a7a19888a6c28436417bc2ba77be9fbce
liangjy <28554768@qq.com>
2016-05-09 00:20:03 +0800

    update

--- rabbitframework-security-pom/rabbitframework-security/src/test/java/com/rabbitframework/security/util/AntPathMatherTest.java
+++ rabbitframework-security-pom/rabbitframework-security/src/test/java/com/rabbitframework/security/util/AntPathMatherTest.java
@@ -4,12 +4,12 @@ import org.apache.shiro.util.AntPathMatcher;
 import org.junit.Test;
 
 public class AntPathMatherTest {
-	private AntPathMatcher antPathMather = new AntPathMatcher();
+    private AntPathMatcher antPathMather = new AntPathMatcher();
 
-	@Test
-	public void testPathMather() {
-		String source = "/login";
-		String pattern = "/login/test";
-		System.out.println(antPathMather.matchStart(pattern, source));
-	}
+    @Test
+    public void testPathMather() {
+        String source = "/login";
+        String pattern = "/login/test";
+        System.out.println(antPathMather.matchStart(pattern, source));
+    }
 }
